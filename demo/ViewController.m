e1d1c16f3b8e79ab71aa9561d1c3f9b8e39df588
zhangcx627 <chenxidouzi@163.com>
2016-10-12 14:26:12 +0800

    master branch commit

--- demo/ViewController.m
+++ demo/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     UILabel *firstCommit = [[UILabel alloc]init];
     firstCommit.text = @"firstCommit";
+    UILabel *secondCommit = [[UILabel alloc]init];
+    secondCommit.text = @"master branch";
 }
 
 - (void)didReceiveMemoryWarning {
