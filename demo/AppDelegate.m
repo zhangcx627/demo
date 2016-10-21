b8046258248ebeaabd7b8f82bc4890f345751202
zhangcx627 <chenxidouzi@163.com>
2016-10-21 11:34:31 +0800

    回滚

--- demo/AppDelegate.m
+++ demo/AppDelegate.m
@@ -17,8 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSString *str = @"master branch";
-    
     return YES;
 }
 
